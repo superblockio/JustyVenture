d1099a38d36b81590ed5fb692e9d089319a4726a
Nathan Swenson <superblockio@me.com>
2012-12-24 16:10:33 -0700

    Added stuff

--- IntroLocation.m
+++ IntroLocation.m
@@ -12,14 +12,20 @@
 
 + (NSString*) arrive
 {
-    return @"You're on an epic adventure where the majority of the coolest things have already happened, but there's no point in telling you about them because you were there. After saving the world and saying teary goodbyes to your hobo sidekick, you part ways. At least you'll always have his cat whistle to remember him by. After walking for a long while and reminiscing through all the good times you can totally remember having, you come to a sudden realization that you have no idea where you are. You find yourself in a dark forrest surrounded by man-eating trees that are trying desperately to convince you that their mouths are where you should sleep for the night. There is a nice, warm-looking cabin to the EAST and a strange clearing to the WEST.";
+    [Player giveItem:@"cat whistle"];
+    return @"You're on an epic adventure where the majority of the coolest things have already happened, but there's no point in telling you about them because you were there. After saving the world and saying teary goodbyes to your hobo sidekick, you part ways. At least you'll always have his cat whistle to remember him by. After walking for a long while and reminiscing through all the good times you can totally remember having, you come to a sudden realization that you have no idea where you are. You find yourself in a dark forest surrounded by man-eating trees that are trying desperately to convince you that their mouths are where you should sleep for the night. There is a nice, warm-looking cabin to the EAST.";
 }
 
 + (NSString*) look:(NSString *)subject
 {
     if (subject == nil) 
     {
-        return @"Boo";
+        return @"Man-eating TREES surround you. Fortunately, they can't move and appear to rely solely on social skills to lure victims to their doom. Even though the trees offer reassurances that their mouths are safe and people sleep there all the time, you can't help but thinking that the CABIN is a competitive option as well.";
+    }
+    
+    else if ([subject isEqualToString:@"trees"] || [subject isEqualToString:@"tree"])
+    {
+        return @"";
     }
     
     return [super look:subject];
