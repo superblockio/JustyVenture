8cdb724a63f9ae0f574626952d86d334791fe1e3
Chad Anderson <pizza29965@neejean.org>
2012-12-24 15:44:42 -0700

    Grammar
    Fixing the wording slightly.

--- IntroLocation.m
+++ IntroLocation.m
@@ -12,7 +12,7 @@
 
 + (NSString*) arrive
 {
-    return @"You're on an epic adventure where the majority of the coolest things have already happened, but there's no point in telling you about them because you were there. After saving the world and saying teary goodbyes to your hobo sidekick, you part ways. At least you'll always have his cat whistle to remember him by. After walking for a long while and reminiscing through all the good times you can totally remember having, you come to a sudden realization that you have no idea where you are. You find yourself in a dark forrest surrounded by man-eating trees that are trying desperately to convince you that their mouths are where you should sleep for the night. There is a nice, warm-looking cabin to the EAST and a strange clearing to the WEST.";
+    return @"You're on an epic adventure where the majority of the coolest things have already happened, but there's no point in telling you about them because you were there. After saving the world and saying teary goodbyes to your hobo sidekick, you part ways. At least you'll always have his cat whistle to remember him by. After walking for a long while and reminiscing through all the good times you can totally remember having, you come to a sudden realization that you have no idea where you are. Looking around you find that your in a dark forrest surrounded by man-eating trees that are trying desperately to convince you that their mouths are where you should sleep for the night. There is a nice, warm-looking cabin to the EAST and a strange clearing to the WEST.";
 }
 
 + (NSString*) look:(NSString *)subject
