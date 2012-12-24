2a515bbd7b6249f2f398d9a38c955d9e0dfc2030
= <=>
2012-12-24 15:30:43 -0700

    Added some intro text

--- IntroLocation.m
+++ IntroLocation.m
@@ -12,14 +12,14 @@
 
 + (NSString*) arrive
 {
-    return @"You find yourself in a forest. You are surrounded by tall, man-eating trees that are trying desperately to convince you that their mouths are where you should sleep for the night.";
+    return @"You're on an epic adventure where the majority of the coolest things have already happened, but there's no point in telling you about them because you were there. After saving the world and saying teary goodbyes to your hobo sidekick, you part ways. At least you'll always have his cat whistle to remember him by. After walking for a long while and reminiscing through all the good times you can totally remember having, you come to a sudden realization that you have no idea where you are. You find yourself in a dark forrest surrounded by man-eating trees that are trying desperately to convince you that their mouths are where you should sleep for the night. There is a nice, warm-looking cabin to the EAST and a strange clearing to the WEST.";
 }
 
 + (NSString*) look:(NSString *)subject
 {
     if (subject == nil) 
     {
-        return @"The trees seem to be old and musty. You could probably outrun them if you hadn't let them surround you. Why did you do that anyway?";
+        return @"BLAH!";
     }
     
     return [super look:subject];
