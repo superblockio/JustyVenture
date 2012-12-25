a1ee3b533a0384f3386c3d2edf3bf9f14d3f961b
Nathan Swenson <superblockio@me.com>
2012-12-25 02:29:19 -0700

    Did pooly stuff

--- PoolLocation.m
+++ PoolLocation.m
@@ -12,16 +12,23 @@
 
 + (NSString*) arrive
 {
-    return @"You reach the bottom of the slide and slide out of a fireplace into a cabin.";
+    return @"I accidentally the arrive text";
 }
 
 + (NSString*) look:(NSString *)subject
 {
     if (subject == nil)
     {
-        return @"";
+        return @"The pool is surrounded by grass and and a few stones, one of which has some writing carved into it. The whole scene seems very serene. There are small bubbles percolating up from the center of the pool. It's pretty bubbly, pretty likely magical. Magical bubbly, you know? Likely?";
     }
-    
+    else if ([subject isEqualToString:@"pool"] || [subject isEqualToString:@"bubbles"])
+    {
+        return @"As you stare deep into the pool, it appears to stare back into your very soul. That's deep, man.";
+    }
+    else if ([subject isEqualToString:@"writing"] || [subject isEqualToString:@"stone"])
+    {
+        return @"The carving reads: \"The Pool of Might. Give of your worldly possesions and receive a fitting reward.\" Seems Legit.";
+    }
     return [super look:subject];
 }
 
@@ -29,7 +36,6 @@
 {
         return [super get:subject];
 }
-
 
 + (NSString*) get:(NSString *)subject
 {
@@ -54,6 +60,21 @@
 
 + (NSString*)wildcardWithVerb:(NSString *)verb subject:(NSString *)subject
 {
+    if ([verb isEqualToString:@"give"] && [Player hasItem:subject])
+    {
+        if ([subject isEqualToString:@"matchbox"])
+        {
+            return @"You throw the matchbox into the pool and get back a matchbox full of wet, useless matches. What did you expect?";
+        }
+        else if ([subject isEqualToString:@"screwdriver"])
+        {
+            return @"You toss the screwdriver into the pool. The pool promptly ejects it back out. While the screwdriver initially appears unchanged, closer inspection reveals that an image of Sonic the Hedgehog has been etched into the plastic handle. You ponder this for a moment, and then chuckle. Well-played, pool, well-played.";
+        }
+    }
+    else if ([verb isEqualToString:@"give"] && ![Player hasItem:subject])
+    {
+        return @"No giving the pool things you don't have.";
+    }
     return [super wildcardWithVerb:verb subject:subject];
 }
 
