2fcba3e1e697e7e5f098528dc0fc81e9ab2d4d82
Chad Anderson <pizza29965@neejean.org>
2012-12-25 15:30:29 -0700

    Endgame
    I added the stuff that allows you to win into the cabin hallway, but
    you feel free to tweak it however you'd like so it functions with the
    actual winning of the game stuff.  Anyway, I believe I've done
    everything that was expected of me, and I might not be around to make
    any more changes.

--- CabinHallwayLocation.m
+++ CabinHallwayLocation.m
@@ -34,6 +34,11 @@
         return @"It almost looks like you could pry this floorboard up if you had a screwdriver or something.";
     }
     return [super look:subject];
+    if ([subject isEqualToString:@"locked door"])
+    {
+        return @"It's an ornate looking door with a keyhole and no door knob.  You must have to find the key somewhere.";
+    }
+    return [super look:subject];
 }
 
 + (NSString*) go:(NSString *)subject
@@ -49,6 +54,13 @@
     if ([subject isEqualToString:@"main room"])
     {
         return [Player setCurrentLocation:@"CabinLocation"];
+    }
+    if ([subject isEqualToString:@"locked door"])
+    {
+        if ([Player hasItem:@"key"])
+            return [self use:@"key"];
+        else
+            return @"The door is locked and you can't get through.";
     }
     return [super get:subject];
 }
@@ -71,6 +83,10 @@
             [Player giveItem:@"murder book"];
             return @"You use the screwdriver to pry up the floorboard, and looking inside you find a little book filled with crossed off names, as if these targets have been eliminated.  You pull it out and tap the floorboard back down into place.";
         }
+    }
+    if ([subject isEqualToString:@"key"])
+    {
+            return @"You unlock the door and step through. Looking around you realize that you're in Nathan's basement and the door has disappeared behind you. Going upstairs you run into Nathan who seems surprised to see you. Oh right, that's me-Uh, wow, were you trapped in my spatial anomaly this whole time? That's really odd. <.< >.>\nAnyway, would you like a ride home?\n\nYou win!";
     }
     return [super use:subject];
 }
