4871ad5bd3b649af66449e63c92d9174db54b511
Chad Anderson <pizza29965@neejean.org>
2012-12-25 02:33:50 -0700

    Hallway Changes
    I think I got everything in the hallway functioning to a bare minimum.

--- CabinHallwayLocation.m
+++ CabinHallwayLocation.m
@@ -12,22 +12,26 @@
 
 + (NSString*) arrive
 {
-    return @"You step through the doorway out into the hallway.  It's a cosy little hallway with a few pictures on the wall.";
+    return @"You step through the doorway out into the hallway.  It's a cosy little hallway with doors leading off it in four different directions.";
 }
 
 + (NSString*) look:(NSString *)subject
 {
     if (subject == nil)
     {
-        return @"";
+        return @"There are hardwood floors, and paintings on the wall of the little hallway. There's a door leading in each direction, one to the bedroom, one to the bathroom, one to the main room, and one that's locked that you can't get through.  You notice the floor is a bit dinged up in a few places.";
     }
-    if ([subject isEqualToString:@"painting"])
+    if ([subject isEqualToString:@"painting"] || [subject isEqualToString:@"paintings"])
     {
-        return @"";
+        return @"They're those paintings you always see in cabins or hunters houses.  Mostly of animals and stuff, not really anything especially interesting. Although you do notice something odd about one of the paintings, and when you shift it to the side you see someone's scribbled, \"The key rests at the top of the vine.\" in small print.";
     }
     if ([subject isEqualToString:@"floor"])
     {
-        return @"";
+        return @"There are various dinges and scratches everywhere, and a floorboard that appears to be loose.";
+    }
+    if ([subject isEqualToString:@"floorboard"])
+    {
+        return @"It almost looks like you could pry this floorboard up if you had a screwdriver or something.";
     }
     return [super look:subject];
 }
@@ -58,6 +62,16 @@
 + (NSString*) use:(NSString *)subject
 {
     if(![Player hasItem:subject]) return [super use:subject];
+    if ([subject isEqualToString:@"screwdriver"])
+    {
+        if ([Player hasItem:@"murder book"])
+            return @"You already opened it up and collected what was to be had.";
+        else
+        {
+            [Player giveItem:@"murder book"];
+            return @"You use the screwdriver to pry up the floorboard, and looking inside you find a little book filled with crossed off names, as if these targets have been eliminated.  You pull it out and tap the floorboard back down into place.";
+        }
+    }
     return [super use:subject];
 }
 
@@ -68,7 +82,7 @@
 
 + (NSString*) whistle
 {
-    return @"";
+    return @"Cats run in through the open bathroom door and scratch at a loose floorboard.  You don't understand where all these cats keep coming from, and what's more confusing is where they're going to...";
 }
 
 + (NSString*)wildcardWithVerb:(NSString *)verb subject:(NSString *)subject
