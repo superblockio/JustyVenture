31c45e524d62fc3ac46275292ba3aac102bbef86
Nathan Swenson <superblockio@me.com>
2012-12-25 11:46:06 -0700

    Added some pool stuff

--- PoolLocation.m
+++ PoolLocation.m
@@ -10,8 +10,11 @@
 
 @implementation PoolLocation
 
+static int _keyItemsInPool;
+
 + (NSString*) arrive
 {
+    _keyItemsInPool = 0;
     return @"I accidentally the arrive text";
 }
 
@@ -19,7 +22,7 @@
 {
     if (subject == nil)
     {
-        return @"The pool is surrounded by grass and and a few stones, one of which has some writing carved into it. The whole scene seems very serene. There are small bubbles percolating up from the center of the pool. It's pretty bubbly, pretty likely magical. Magical bubbly, you know? Likely?";
+        return @"The pool is surrounded by grass, some mushrooms, and and a few stones, one of which has some writing carved into it. The whole scene seems very serene. There are small bubbles percolating up from the center of the pool. It's pretty bubbly, pretty likely magical. Magical bubbly, you know? Likely?";
     }
     else if ([subject isEqualToString:@"pool"] || [subject isEqualToString:@"bubbles"])
     {
@@ -28,6 +31,10 @@
     else if ([subject isEqualToString:@"writing"] || [subject isEqualToString:@"stone"])
     {
         return @"The carving reads: \"The Pool of Might. Give of your worldly possesions and receive a fitting reward.\" Seems Legit.";
+    }
+    else if ([subject isEqualToString:@"mushroom"] || [subject isEqualToString:@"mushrooms"])
+    {
+        return @"They look like very gettable mushrooms!";
     }
     return [super look:subject];
 }
@@ -39,6 +46,10 @@
 
 + (NSString*) get:(NSString *)subject
 {
+    if ([subject isEqualToString:@"mushroom"] || [subject isEqualToString:@"mushrooms"])
+    {
+        [Player giveItem:@"mushroom"];
+    }
     return [super get:subject];
 }
 
@@ -71,6 +82,33 @@
         else if ([subject isEqualToString:@"screwdriver"])
         {
             return @"You toss the screwdriver into the pool. The pool promptly ejects it back out. While the screwdriver initially appears unchanged, closer inspection reveals that an image of Sonic the Hedgehog has been etched into the plastic handle. You ponder this for a moment, and then chuckle. Well-played, pool, well-played.";
+        }
+        else if ([subject isEqualToString:@"pencil"])
+        {
+            [Player removeItem:@"pencil"];
+            return @"Swirling black lines expand across the surface of the pool from the point your pencil entered the water. These lines twist and intersect until they form... an image of Fluttershy! This is by far the cutest Fluttershy depiction you've seen in your entire life. Unfortunately, the black lines soon start to dissipate, leaving you with nothing but a memory. This has taught you a valuable lesson: next time, use a pen.";
+        }
+        else if ([subject isEqualToString:@"pine cone"])
+        {
+            [Player removeItem:@"pine cone"];
+            return @"You throw in the pine cone, and with a great blue flash the pool launches a large object high into the air. You have to dodge out of the way to keep it from hitting you on its way down. After this horrific incident of terror is over, you take a look and see that it is a giant 3D cone! You can hear a soft sobbing noise coming from it, as though the cone is pining away for a long-lost loved one. 3D primatives have emotions too! These emotions are short-lived, however, as the cone wriggles its way back into the pool and gets dispersed into its depths.";
+        }
+        else if ([subject isEqualToString:@"shaving cream"])
+        {
+            [Player removeItem:@"shaving cream"];
+            [Player giveItem:@"beard"];
+            return @"With a mighty throw, you hurl the shaving cream into the water. The pool pauses for a moment, as if contemplating your hostility towards shaving. Then a jet of water surges out and hits you square in the head, knocking you flat on your back. As you reach for your face to wipe the water away, your hand brushes against long, flowing hair. It appears that you now have a wizard-style beard! You feel much more distinguished now, as if wisdom is oosing out of the tip of each hair. You feel as though you can solve any puzzle with ease. So go ahead, beardy, impress me!";
+        }
+        else if ([subject isEqualToString:@"pillow"])
+        {
+            [Player removeItem:@"pillow"];
+            return @"A large, ornate bed materializes on the surface of the water. It has red curtains, a solid wood frame with fancy carvings in it, and a fluffy matress to send you to heaven. You hop aboard this extremely comfortable bed, almost forgetting that it is still suspended in a pool of water. This soon changes as the cold water reaches your feet and works its way to the rest of your body. You soon find yourself abandoning ship and swimming back to shore as the bed slowly sinks into the abyss. You wonder what the pool was trying to accomplish by doing all this.";
+        }
+        else if ([subject isEqualToString:@"toothbrush"])
+        {
+            [Player removeItem:@"toothbrush"];
+            [Player giveItem:@"electric eel"];
+            return @"You throw in the toothbrush and out pops an electric eel! A deep voice booms from within the heart of the pool: \"I was going to give you an electric toothbrush but then I started thinking of electric eels because I'm a body of water.\" You rejoice at your good fortune and promptly stuff the eel into your pants, even though it kinda stings your skin.";
         }
     }
     else if ([verb isEqualToString:@"give"] && ![Player hasItem:subject])
