17659598ccba2140221cfda7d09b5a39be798178
Nathan Swenson <superblockio@me.com>
2012-12-26 17:06:46 -0700

    Changed fireplacey stuff a bit

--- CabinFireplaceLocation.m
+++ CabinFireplaceLocation.m
@@ -12,14 +12,14 @@
 
 + (NSString*) arrive
 {
-    return @"You tumble down another hole and land in a heap in a cramped little space. Moving into a crouch you see a crawl space in front of you that appears to lead into some sort of cabin, and an old man squatting next to you.";
+    return @"You tumble down another hole and land on a heap of charcoal in a cramped little space. Crouching down you see a crawl space in front of you that appears to lead into some sort of cabin. An old man is squatting next to you.";
 }
 
 + (NSString*) look:(NSString *)subject
 {
     if (subject == nil)
     {
-        return @"The crawl space looks small, but you think you'll be able to fit. The old man looks to be playing some sort of game on his phone and isn't really paying all that much attention to you. He's sitting next to a pile of salt.";
+        return @"The crawl space leading into the cabin looks small, but you think you'll be able to fit. The old man looks to be playing some sort of game on his phone and isn't really paying all that much attention to you. He's sitting next to a pile of salt.";
     }
     if ([subject isEqualToString:@"salt"])
     {
@@ -61,7 +61,7 @@
 {
     if ([subject isEqualToString:@"old man"])
     {
-        return @"Why won't you leave me alone! Every year around Christmas you show up on one of your adventures and interupt my peace!  Can't an old man crouch is strange places and play with his phone in peace!?";
+        return @"Why won't you leave me alone! Every year around Christmas you show up on one of your crazy adventures and interupt my peace!  Can't an old man crouch is strange places and play with his phone in peace!?";
     }
     return [super talk:subject];
 }
